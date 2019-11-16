e203fdae7bef048f01b8cfeb013c31c97c2118ab
Ogabek Yuldashov <ogabek.yuldashov@gmail.com>
2019-11-16 16:49:21 -0600

    Initial commit

---
+++ src/main/java/ea/sof/ms_bgscoring/MsBgscoringApplication.java
@@ -0,0 +1,13 @@
+package ea.sof.ms_bgscoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBgscoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsBgscoringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ea/sof/ms_bgscoring/MsBgscoringApplicationTests.java
@@ -0,0 +1,13 @@
+package ea.sof.ms_bgscoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBgscoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
