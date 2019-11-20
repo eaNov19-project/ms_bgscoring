55e56e7f2546caa11c6cfd0cc10dbcf9ad564609
Ogabek Yuldashov <ogabek.yuldashov@gmail.com>
2019-11-20 13:58:09 -0600

    SubsAnswerVoted bug fix

--- src/main/java/ea/sof/ms_bgscoring/kafka/SubsAnswerVoted.java
+++ src/main/java/ea/sof/ms_bgscoring/kafka/SubsAnswerVoted.java
@@ -6,7 +6,8 @@ import ea.sof.ms_bgscoring.repository.QuestionRepository;
 import ea.sof.ms_bgscoring.services.AnswersService;
 import ea.sof.shared.entities.AnswerEntity;
 import ea.sof.shared.queue_models.AnswerQueueModel;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,10 +15,10 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 import java.util.Comparator;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-@Slf4j
 public class SubsAnswerVoted {
 
     @Autowired
@@ -26,9 +27,11 @@ public class SubsAnswerVoted {
     @Autowired
     AnswersService answersService;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(SubsAnswerVoted.class);
+
     @KafkaListener(topics = "${topicAnswerVoted}", groupId = "${subsAnswerVoted}")
     public void listener(String message) {
-        log.info("\nSubsAnswerVoted :: New message from topic 'topicAnswerVoted': " + message);
+        LOGGER.info("\nSubsAnswerVoted :: New message from topic 'topicAnswerVoted': " + message);
 
         // 1. Getting data from request
         AnswerQueueModel answerQueueModel = null;
@@ -36,14 +39,15 @@ public class SubsAnswerVoted {
             Gson gson = new Gson();
             answerQueueModel = gson.fromJson(message, AnswerQueueModel.class);
         } catch (Exception ex) {
-            log.warn("SubsAnswerVoted :: Failed to convert Json: " + ex.getMessage());
+            LOGGER.warn("SubsAnswerVoted :: Failed to convert Json: " + ex.getMessage());
+            return;
         }
 
         // 2. Retrieving question from DB
         String questionId = answerQueueModel.getQuestionId();
         QuestionEntity questionEntity = questionRepository.findById(questionId).orElse(null);
         if (questionEntity == null) {
-            log.warn("SubsAnswerVoted :: Failed to retrieve Question from DB.");
+            LOGGER.warn("SubsAnswerVoted :: Failed to retrieve Question from DB.");
             return;
         }
 
@@ -51,36 +55,30 @@ public class SubsAnswerVoted {
         // 3. Retrieving full answer by ID
         ResponseEntity<AnswerEntity> answerResponse = answersService.getAnswer(answerQueueModel.getId());
         if (answerResponse.getStatusCode() != HttpStatus.OK) {
-            log.warn("SubsAnswerVoted :: Failed to retrieve Answer for.");
+            LOGGER.warn("SubsAnswerVoted :: Failed to retrieve Answer for.");
             return;
         }
         AnswerEntity answerEntity = answerResponse.getBody();
 
 
-        // 3. adding the answer to the top list if there is space
+        // 4. get the top answers from service
+        List<AnswerEntity> answerEntities = null;
         try {
+            ResponseEntity<List<AnswerEntity>> answersServiceResp = answersService.getTop5AnswersByQuestionId(questionId);
+            answerEntities = answersServiceResp.getBody();
+        } catch (Exception ex){
+            LOGGER.warn("SubsAnswerVoted :: Failed to retrieve top 5 answers for question with ID: ." + questionId);
+            return;
+        }
 
-            if (questionEntity.getTopAnswers().size() < 5) {
-                questionEntity.addAnswer(answerEntity);
-                log.info("SubsAnswerVoted :: Answer added to the top 5 answers list of question");
-            } else {
-                for (int i = questionEntity.getTopAnswers().size() - 1; i >= 0; i--) {
-                    if (questionEntity.getTopAnswers().get(i).getVotes() < answerQueueModel.getVotes()) {
-                        questionEntity.getTopAnswers().remove(questionEntity.getTopAnswers().get(i));
-                        questionEntity.addAnswer(answerEntity);
-                        log.info("SubsAnswerVoted :: Answer was replaced in the top 5 answers list of questions");
-                        break;
-                    }
-                }
-            }
-            questionEntity.getTopAnswers().stream()
-                    .sorted(Comparator.comparingInt(AnswerEntity::getVotes).reversed())
-                    .collect(Collectors.toList());
+        // 5. Update the top answers
+        try {
+            questionEntity.setTopAnswers(answerEntities);
             questionRepository.save(questionEntity);
-            log.info("SubsAnswerVoted :: Top 5 answers list updated successfully");
+            LOGGER.info("SubsAnswerVoted :: Top 5 answers list updated successfully");
 
         } catch (Exception ex) {
-            log.warn("SubsAnswerVoted :: Failed to save Entity: " + ex.getMessage());
+            LOGGER.warn("SubsAnswerVoted :: Failed to save Entity: " + ex.getMessage());
         }
     }
 
